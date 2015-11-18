1bcbbf1c06a7dec6b7b72356f63da8b505a263f8
Bradley Golden <bradleygolden@10-107-214-71.vlan895.client.wireless.uic.edu>
2015-11-18 17:42:37 -0600

    added query and scalar query methods, provided examples in main

--- src/DataTier.java
+++ src/DataTier.java
@@ -36,17 +36,39 @@ public class DataTier {
 	 * @param query (required) A SoQL query
 	 * @return A JSONArray object containing the contents of the provided SoQL query.
 	 */
-	private JSONArray executeQuery(String query)
+	public JSONArray executeQuery(String query)
 	{
 		query = baseUrl + query;
 		return urlToJSONArray(query);
 	}
 	
 	/**
+	 * Executes a scaler SoQL query against the SODA API.
+	 * 
+	 * @param query (required) Requested query to execute against the database.
+	 * @param key (required) The name of the key you wish to return. For example,
+	 * your key in the query $select=count(some_key) AS count will be count or 
+	 * your key in the query $select=aka_name='SUBWAY' will be aka_name.
+	 * @return Result of the given query provided the given key. Throws RuntimeException on invalid key.
+	 */
+	public Object executeScalarQuery(String query, String key)
+	{
+		query = baseUrl + query;
+		try
+		{
+			return urlToJSONArray(query).getJSONObject(0).get(key);
+		}
+		catch(JSONException ex)
+		{
+			throw new RuntimeException(String.format("Invalid key: %s", key));
+		}
+	}
+	
+	/**
 	 * Takes a given URL, fetches the contents from the URL, and converts those contents to a JSONArray object.
 	 * 
 	 * @param urlString (required) A url that contains text in JSON format.
-	 * @return JSONArray object filled with data from the given urlString or NULL on failure.
+	 * @return JSONArray object filled with data from the given urlString or RuntimeException on failure.
 	 */
 	private JSONArray urlToJSONArray(String urlString)
 	{
@@ -68,14 +90,16 @@ public class DataTier {
 			{
 				jsonString += " " + s.next(); // read each word with a space in between
 			}
+			
+			s.close(); // close the scanner
 		}
 		
 		// there was an issue with the provided url
 		// it may be the case that the facility name provided doesn't
 		// exists in the database
-		catch(IOException ex) {
-			ex.printStackTrace();
-			return null;
+		catch(IOException ex) 
+		{
+			throw new RuntimeException(String.format("Invalid SoQL Query: %s. Please check the query you provided.", urlString));
 		}
 		
 		// attempt to build the JSONArray object
@@ -87,10 +111,9 @@ public class DataTier {
 		
 		// an unknown issue occurred here
 		// display the error return null
-		catch (JSONException e) 
+		catch (JSONException ex) 
 		{
-		    e.printStackTrace();
-		    return null;
+		    throw new RuntimeException(String.format("JSON at the given URL could not be parsed correctly: %s", urlString));
 		}
 		
 		return json;
@@ -108,8 +131,14 @@ public class DataTier {
 		DataTier dt = new DataTier();
 		ArrayList<JSONObject> jsonList;
 		
-		// queryByName
-		JSONArray json = dt.executeQuery("$where=aka_name='MAKOTO'");
+		//
+		// executeQuery
+		//
+		JSONArray json = dt.executeQuery("$select=aka_name,results,violations&$where=aka_name='MAKOTO'");
+		
+		System.out.println("--------------------------------");
+		System.out.println("     executeQuery Example       ");
+		System.out.println("--------------------------------");
 		
 		try
 		{
@@ -117,9 +146,26 @@ public class DataTier {
 			System.out.println(json.getJSONObject(0).get("results")); // retrieve result: pass, fail, etc.
 			System.out.println(json.getJSONObject(0).get("violations")); // retrieve violation details
 		}
+		catch (NullPointerException ex)
+		{
+			System.out.println(ex.toString());
+		}
 		catch (JSONException ex)
 		{
-			System.out.println("Failed somewhere...");
+			System.out.println(ex.toString());
 		}
+		
+		//
+		// executeScalarQuery
+		//
+		String result = (String) dt.executeScalarQuery("$select=aka_name,count(aka_name)%20AS%20count&$where=aka_name=%27SUBWAY%27&$group=aka_name", "count");
+		String result2 = (String) dt.executeScalarQuery("$select=aka_name,count(aka_name)%20AS%20count&$where=aka_name=%27SUBWAY%27&$group=aka_name", "aka_name");
+			
+		System.out.println("--------------------------------------");
+		System.out.println("     executeScalarQuery Example       ");
+		System.out.println("--------------------------------------");
+		
+		System.out.println(result); // count
+		System.out.println(result2); // aka_name
 	}
 }
